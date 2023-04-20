d05938b27ad76469d433a78047b05734b287238f
Martin3130 <nfu40923130@gmail.com>
2023-04-20 14:37:13 +0800

    revise content

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'Home', 'text': '組員: \n 40923115 吳圳吉 \n 40923130 林祐詮 \n 40923235 詹侑儒 \n \n 倉儲: \n \xa0 \xa0 \xa0   https://github.com/mdecd2023/2b2-pj2bg15 \n 網站: \n \xa0 \xa0 \xa0   https://mdecd2023.github.io/2b2-pj2bg15/content/index.html', 'tags': '', 'url': 'Home.html'}]};
